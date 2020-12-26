8ae80c07562433afff31343e443acf48931a81cb
Igor Tuag <igortuag@gmail.com>
2020-12-26 08:48:39 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9c4ff0dc0987d3a3d839ec6e4e72b99e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '51df38b7-3eb2-4e99-a05f-6bb585823ed0'
+};
