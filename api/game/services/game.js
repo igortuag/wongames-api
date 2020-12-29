6b44a8c36a80c53baca59c564e4507fc75233007
Igor Tuag <igortuag@gmail.com>
2020-12-29 07:24:07 -0300

    install jsdom and scraping ratingElement

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -2,12 +2,23 @@
 
 const axios = require('axios');
 
+async function getGameInfo(slug) {
+  const jsdom = require('jsdom');
+  const { JSDOM } = jsdom;
+  const body = await axios.get(`https://www.gog.com/game/${slug}`)
+  const dom = new JSDOM(body.data);
+
+  const ratingElement = dom.window.document.querySelector('.age-restrictions__icon use')
+
+  console.log(ratingElement)
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
 
     const { data : { products } } = await axios.get(gogApiUrl)
 
-    console.log(products[0])
+    await getGameInfo(products[0].slug);
   }
 };
