ae82eda34108e31dd1d0d454e68135ea4b7ac259
Tuag <igortuag@gmail.com>
2021-12-10 07:00:17 -0300

    add text template

--- config/email-templates/order.js
+++ config/email-templates/order.js
@@ -1,6 +1,17 @@
-const subject = "Your order has been placed";
+const subject = "Order at Won Games";
 
-const text = ``;
+const text = `
+  Hi <%= user.username %>, thanks for buying at Won Games!
+    Follow the info of your order:
+    Card Information:
+    Card brand: <%= payment.card_brand %>
+    Card number: **** **** **** <%= payment.card_last4 %>
+    Total: <%= payment.total %>
+    Games:
+    <% _.forEach(games, function(game) { %>
+      <%= game.name %> - Price: $<%= game.price %>
+    <% }); %>
+`;
 
 const html = ``;
 
