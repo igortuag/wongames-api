6be16eeec716f004ac246f5eed7f8b320f3feb16
Tuag <igortuag@gmail.com>
2021-12-10 07:00:41 -0300

    add html to template

--- config/email-templates/order.js
+++ config/email-templates/order.js
@@ -13,7 +13,23 @@ const text = `
     <% }); %>
 `;
 
-const html = ``;
+const html = `
+<p>Hi <%= user.username %>, thanks for buying at Won Games!</p>
+  <p>Follow the info of your order:</p>
+  <h3>Card Information</h3>
+  <ul>
+    <li><strong>Card brand:</strong> <%= payment.card_brand %></li>
+    <li><strong>Card number:</strong> **** **** **** <%= payment.card_last4 %></li>
+  </ul>
+  <h3>Total: <%= payment.total %></h3>
+  <hr />
+  <h3>Games</h3>
+  <ul>
+    <% _.forEach(games, function(game) { %>
+			<li><a href="http://localhost:3000/game/<%= game.slug %>"><%= game.name %></a> - Price: <strong>$<%= Number(game.price).toFixed(2) %></strong></li>
+		<% }); %>
+  </ul>
+`;
 
 module.exports = {
   subject,
