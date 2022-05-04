0c0d784894bfc3f753992779125aea378141c6d3
Tuag <igortuag@gmail.com>
2022-05-04 07:03:49 -0300

    fix database config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -13,11 +13,9 @@ module.exports = ({ env }) => ({
         database: config.database,
         username: config.user,
         password: config.password,
-        ssl: env.bool("DATABASE_SSL")
-          ? {
-              rejectUnauthorized: false,
-            }
-          : false,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
       options: {
         ssl: true,
